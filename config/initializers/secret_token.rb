d804408d01bf680798f7c6032cca4e7df45f5d6c
Jayson Perry <jayson.t.perry@gmail.com>
2013-12-06 09:43:31 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifeforcechiropractic::Application.config.secret_key_base = 'ce13f70b6e09e1e995df29c7a801a29665c7fc19e648558abc8c0bc740b5d99ce23bba05fe647d61716818fa259e1442b4b90f5b7dabb3fcc7befde724f3adf7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lifeforcechiropractic::Application.config.session_store :cookie_store, key: '_lifeforcechiropractic_session'
